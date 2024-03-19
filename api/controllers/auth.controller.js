18122c6706ab26d1f2846dab216f0603f284f98f
ADITYA GUPTA <ag7023587@gmail.com>
2024-03-20 00:53:05 +0530

    complete sign in ui

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -42,7 +42,7 @@ export const signin = async(req,res,next)=>{
     const {password: pass, ...rest} = validUser._doc;
     res
     .cookie('access_token', token, {httpOnly: true})
-    .status(200).json(validUser)
+    .status(200).json(rest)
 }
 catch(err){
     next(err);
