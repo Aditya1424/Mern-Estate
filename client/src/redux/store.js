a8269ec352f87773cd9fb6fffd21809369467409
ADITYA GUPTA <ag7023587@gmail.com>
2024-03-20 18:49:54 +0530

    add redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,26 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers,configureStore } from '@reduxjs/toolkit'
 import userReducer from './user/userSlice'
+import {persistReducer, persistStore} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
+
+const rootReducer = combineReducers({user: userReducer});
+
+const persistConfig=  {
+    key: 'root',
+    storage,
+    version : 1
+}
+// Persist Reducer used to store the user data in the local storage of the browser 
+// such that if we reload our page, the details of the user still exist
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 
 export const store = configureStore({
-  reducer: {user: userReducer},
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({
     serializableCheck: false
   })
 })
 
+export const persistor = persistStore(store);
