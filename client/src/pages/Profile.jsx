39ca48f945b4c14d81e11d0fac16889249b12245
ADITYA GUPTA <ag7023587@gmail.com>
2024-04-11 18:13:32 +0530

    complete update listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -236,7 +236,7 @@ const handleSubmit= async(e)=>{
         <div className='flex flex-col gap-4'> 
           <h1 className='text-center mt-7 text-2xl font-semibold'>Your Listings</h1>
           {userListings.map((listing)=> 
-        <div key={listing._id} className='border rounded-lg p-3 flex justify-between items-center gap-5'>
+        <div key={listing._id} className='border rounded-lg p-3 flex justify-between items-center gap-5'> 
              
              <Link to= {`/listing/${listing._id}`}>
 
@@ -244,12 +244,16 @@ const handleSubmit= async(e)=>{
              </Link>
 
              <Link className='text-slate-700 font-semibold flex-1 hover:underline truncate' to={`/listing/${listing._id}`}>
+              
                 <p >{listing.name}</p>
              </Link>
 
              <div className='flex flex-col items-center'>
                 <button onClick={()=> handleListingDelete(listing._id)} className='text-red-700'>Delete</button>
+                <Link to={`/update-listing/${listing._id}`}>
                 <button onClick={()=>handleListingUpdate()} className='text-green-700'>Edit</button>
+                </Link>
+
             </div>
 
         </div>)}
