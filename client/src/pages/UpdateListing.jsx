c8ff86d8b704dd199feef717dc028a008b78476a
ADITYA GUPTA <ag7023587@gmail.com>
2024-04-11 21:32:39 +0530

    adding contact landlord functionality to the listing page

--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -6,7 +6,7 @@ import { app } from '../firebase';
 import { useSelector } from 'react-redux';
 
 import { useNavigate , useParams} from 'react-router-dom';
-export default function CreateListing() {
+export default function UpdateListing() {
 
     const navigate = useNavigate();
     const params = useParams();
