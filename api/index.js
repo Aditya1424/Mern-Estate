b7832c7d799503d1da3acae3dbf3d17bfe9cfde7
ADITYA GUPTA <ag7023587@gmail.com>
2024-04-14 02:25:24 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
 import listingRouter from './routes/listing.route.js'
 import cookieParser from 'cookie-parser'
+import path from 'path';
 dotenv.config();
 
 const app = express();
@@ -22,9 +23,17 @@ mongoose
     console.log(err)
 })
 
+const __dirname = path.resolve();
+
 app.use('/api/user', userRouter)
 app.use('/api/auth', authRouter)
 app.use('/api/listing', listingRouter)
+
+app.use(express.static(path.join(__dirname,'/client/dist')))
+
+app.use('*', (req,res)=>{
+    res.sendFile(path.join(__dirname,'client','dist','index.html'));
+})
 
 app.use((err, req,res, next)=>{
     const statusCode= err.statusCode || 500;
